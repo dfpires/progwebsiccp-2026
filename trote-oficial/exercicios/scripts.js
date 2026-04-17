2e3aadf6172074a41e129d52f1ade008f93144de
Daniel <dfpires@gmail.com>
2026-04-16 21:39:55 -0300

    exe2

--- trote-oficial/exercicios/scripts.js
+++ trote-oficial/exercicios/scripts.js
@@ -3,3 +3,15 @@ function churras(){
     let kgs = (qtdePessoas * 400) / 1000 // transforma gramas em kilos
     document.getElementById("resultadoChurras").innerText = "Total de carne " + kgs + " Kg"
 }
+function febre(){
+    // obter a temperatura digitada pelo usuário
+    let temperatura = Number(document.getElementById("temperatura").value)
+    // verifica o estado febril
+    if (temperatura >= 37.5){
+        // mostra o resultado de estado febril ao usuário
+        document.getElementById("resultadoFebril").innerText = "Está com febre"
+    }
+    else {
+        document.getElementById("resultadoFebril").innerText = "Temperatura normal"
+    }
+}
